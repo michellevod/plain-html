03ca0d2a31cf03fd6643f2d299c8d9fc8a9e100e
michellevod <michelle.nestler@vodafone.com>
2025-11-10 14:11:24 +0100

    Javascript Color: Change, Toggle and Switch! :) Funktioniert.

--- 22.10 - JSArray/js2210.js
+++ 22.10 - JSArray/js2210.js
@@ -1,21 +1,71 @@
-const songs = [" Dynamite - Taio Cruz " , " Atemlos - Helene Fischer" , " Birds of a feather - Billie Eilish " , " Hallo - Susi " , " Tschuess - Daniel" , " Ciao - Michelle "]; 
+const songs = [
+  ' Dynamite - Taio Cruz ',
+  ' Atemlos - Helene Fischer',
+  ' Birds of a feather - Billie Eilish ',
+  ' Hallo - Susi ',
+  ' Tschuess - Daniel',
+  ' Ciao - Michelle '
+]
 
-document.getElementById("playList").innerHTML = songs;
+document.getElementById('playList').innerHTML = songs
 
+let text = ''
+let ranking
 
-let text ="";
-let ranking;
-
-for (let i = 0; i<songs.length; i++){
-    ranking= i;
-    text += "<span class`songRanked`>"+ ranking+1 +".) Song Name: " + songs[i] + "<br>";
+for (let i = 0; i < songs.length; i++) {
+  ranking = i
+  text +=
+    '<span class`songRanked`>' +
+    ranking +
+    1 +
+    '.) Song Name: ' +
+    songs[i] +
+    '<br>'
 }
 
-document.getElementById("playList2").innerHTML = text;
+document.getElementById('playList2').innerHTML = text
 // -----------
-var button = document.querySelector('button');
-var box = document.getElementById('changeMe');
 
-button.onlick = function changeColor() {
-    box.style.background = 'red';
+var changeBox = document.getElementById('changeMe')
+var changeButton = document.getElementById('buttonChangeMe')
+
+changeButton.onclick = function changeColor () {
+  changeBox.style.background = 'red'
 }
+
+var toggleBox = document.getElementById('toggleMe')
+var toggleButton = document.getElementById('buttonToggleMe')
+
+toggleButton.onclick = function toggleColor () {
+  if (toggleBox.style.background === 'yellow') {
+    toggleBox.style.background = 'green'
+  } else {
+    toggleBox.style.background = 'yellow'
+  }
+}
+
+var switchBox = document.getElementById('switchMe')
+switchBox.style.background = 'black';
+var switchButton = document.getElementById('buttonSwitch')
+
+function switchColor() {
+  switch (switchBox.style.background) {
+    case 'black':
+    switchBox.style.background = 'orange';
+    break;
+
+    case 'orange':
+      switchBox.style.background = 'aqua';
+      break;
+
+      case 'aqua':
+      switchBox.style.background = 'green';
+      break;
+
+      case 'green':
+        switchBox.style.background = 'black';
+        break;
+  }
+}
+
+switchButton.onclick = switchColor;
