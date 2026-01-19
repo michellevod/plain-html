20cdd36b6966cba0f6131b19569d8e432bb87083
michellevod <michelle.nestler@vodafone.com>
2026-01-19 11:48:18 +0100

    productCards are implemented with individual content -> tba individual img

--- MyMixxMichelle/konfigurator/myMixx.js
+++ MyMixxMichelle/konfigurator/myMixx.js
@@ -1,56 +1,636 @@
-// PRODUCT CARDS INSTALL //
-const productCardTemplate = `
-  <div class="productCard2">
+// PRODUCT CARDS DATA//
+const basisCerealien = [
+  {
+    id: "basis-1",
+    name: "Zarte Haferflocken",
+    weight: "500 g",
+    price: 2.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-2",
+    name: "Großblättrige Haferflocken",
+    weight: "500 g",
+    price: 2.70,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-3",
+    name: "Dinkelflocken",
+    weight: "400 g",
+    price: 3.20,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-4",
+    name: "Quinoaflocken",
+    weight: "300 g",
+    price: 4.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-5",
+    name: "Buchweizenflocken",
+    weight: "300 g",
+    price: 4.20,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-6",
+    name: "Hirseflocken",
+    weight: "300 g",
+    price: 3.80,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-7",
+    name: "Vollkorn-Reisflocken",
+    weight: "300 g",
+    price: 3.90,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-8",
+    name: "Gerstenflocken",
+    weight: "400 g",
+    price: 3.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-9",
+    name: "Maisgrieß (Polenta)",
+    weight: "500 g",
+    price: 3.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-10",
+    name: "Haferschrot",
+    weight: "400 g",
+    price: 2.80,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-11",
+    name: "Gersten- und Roggenschrot",
+    weight: "400 g",
+    price: 3.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-12",
+    name: "Amaranth-Pops",
+    weight: "200 g",
+    price: 4.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-13",
+    name: "Cornflakes (zuckerfrei & Honig)",
+    weight: "300 g",
+    price: 3.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-14",
+    name: "Crunchy Müsli (Schoko, Nuss, Honig)",
+    weight: "350 g",
+    price: 4.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-15",
+    name: "Rice Krispies",
+    weight: "300 g",
+    price: 3.80,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-16",
+    name: "Weizenkleie",
+    weight: "200 g",
+    price: 2.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-17",
+    name: "Geröstete Haferflakes",
+    weight: "300 g",
+    price: 3.20,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  },
+  {
+    id: "basis-18",
+    name: "Müsliriegelstückchen",
+    weight: "150 g",
+    price: 3.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "basis-CEREAL"
+  }
+];
+const getrockneteFruechte = [
+  {
+    id: "getr-1",
+    name: "Cranberries",
+    weight: "200 g",
+    price: 4.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-2",
+    name: "Rosinen",
+    weight: "250 g",
+    price: 3.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-3",
+    name: "Goji-Beeren",
+    weight: "150 g",
+    price: 6.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-4",
+    name: "Mangostückchen",
+    weight: "150 g",
+    price: 5.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-5",
+    name: "Ananasstücke",
+    weight: "150 g",
+    price: 4.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-6",
+    name: "Apfelringe",
+    weight: "100 g",
+    price: 3.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-7",
+    name: "Bananenchips",
+    weight: "200 g",
+    price: 2.80,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-8",
+    name: "Datteln",
+    weight: "200 g",
+    price: 4.20,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-9",
+    name: "Feigen",
+    weight: "200 g",
+    price: 4.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-10",
+    name: "Erdbeerscheiben",
+    weight: "100 g",
+    price: 6.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-11",
+    name: "Heidelbeeren",
+    weight: "150 g",
+    price: 5.50,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-12",
+    name: "Kirschen",
+    weight: "150 g",
+    price: 5.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  },
+  {
+    id: "getr-13",
+    name: "Aprikosen",
+    weight: "200 g",
+    price: 4.00,
+    image: "img/haferflockenplaceholder.png",
+    category: "getrocknete-Fr"
+  }
+];
+const nuesseSamen = [
+  {
+    id: "nuesse-1",
+    name: "Mandeln (geröstet, gehackt, blättrig)",
+    weight: "150 g",
+    price: 4.00,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-2",
+    name: "Walnüsse",
+    weight: "150 g",
+    price: 4.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-3",
+    name: "Haselnüsse",
+    weight: "150 g",
+    price: 4.20,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-4",
+    name: "Pistazien",
+    weight: "100 g",
+    price: 5.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-5",
+    name: "Cashewkerne",
+    weight: "150 g",
+    price: 4.80,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-6",
+    name: "Pekanüsse",
+    weight: "150 g",
+    price: 5.00,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-7",
+    name: "Sonnenblumenkerne",
+    weight: "200 g",
+    price: 2.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-8",
+    name: "Kürbiskerne",
+    weight: "150 g",
+    price: 3.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-9",
+    name: "Chiasamen",
+    weight: "100 g",
+    price: 3.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-10",
+    name: "Leinsamen",
+    weight: "200 g",
+    price: 2.50,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-11",
+    name: "Hanfsamen",
+    weight: "100 g",
+    price: 4.00,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  },
+  {
+    id: "nuesse-12",
+    name: "Sesamsamen",
+    weight: "200 g",
+    price: 2.80,
+    image: "img/nuesse-placeholder.png",
+    category: "Nuesse-Samen"
+  }
+];
+const superFoods = [
+  {
+    id: "super-1",
+    name: "Kakaonibs",
+    weight: "100 g",
+    price: 4.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-2",
+    name: "Matcha-Pulver",
+    weight: "50 g",
+    price: 8.00,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-3",
+    name: "Maca-Pulver",
+    weight: "100 g",
+    price: 5.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-4",
+    name: "Baobab-Pulver",
+    weight: "100 g",
+    price: 6.00,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-5",
+    name: "Spirulina",
+    weight: "50 g",
+    price: 7.00,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-6",
+    name: "Acai-Beeren",
+    weight: "100 g",
+    price: 8.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-7",
+    name: "Lucuma-Pulver",
+    weight: "100 g",
+    price: 5.00,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-8",
+    name: "Weizengraspulver",
+    weight: "100 g",
+    price: 6.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-9",
+    name: "Gojipulver",
+    weight: "100 g",
+    price: 7.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-10",
+    name: "Kokosflocken",
+    weight: "150 g",
+    price: 3.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  },
+  {
+    id: "super-11",
+    name: "Trockene Maulbeeren",
+    weight: "150 g",
+    price: 5.50,
+    image: "img/superfood-placeholder.png",
+    category: "super-Foods"
+  }
+];
+const extraSuess = [
+  {
+    id: "extra-1",
+    name: "Dunkle Schokoladenstücke",
+    weight: "150 g",
+    price: 3.80,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-2",
+    name: "Weiße Schokoladenstücke",
+    weight: "150 g",
+    price: 3.80,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-3",
+    name: "Karamellstückchen",
+    weight: "150 g",
+    price: 4.00,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-4",
+    name: "Kokoschips",
+    weight: "150 g",
+    price: 3.50,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-5",
+    name: "Honig",
+    weight: "250 g",
+    price: 6.00,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-6",
+    name: "Ahornsirup",
+    weight: "250 ml",
+    price: 8.00,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  },
+  {
+    id: "extra-7",
+    name: "Agavendicksaft",
+    weight: "250 ml",
+    price: 4.50,
+    image: "img/extra-placeholder.png",
+    category: "extra-Su"
+  }
+];
+const fluessigkeiten = [
+  {
+    id: "fluessig-1",
+    name: "Hafermilch",
+    weight: "1 l",
+    price: 1.85,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-2",
+    name: "Mandelmilch",
+    weight: "1 l",
+    price: 2.25,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-3",
+    name: "Kokosmilch",
+    weight: "1 l",
+    price: 2.25,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-4",
+    name: "Sojamilch",
+    weight: "1 l",
+    price: 1.75,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-5",
+    name: "Reismilch",
+    weight: "1 l",
+    price: 1.85,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-6",
+    name: "Vollmilch",
+    weight: "1 l",
+    price: 1.55,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-7",
+    name: "Joghurt (natur, griechisch)",
+    weight: "250 ml",
+    price: 0.95,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-8",
+    name: "Wasser (sehr weich und mikrofiltriert)",
+    weight: "1 l",
+    price: 1.85,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  },
+  {
+    id: "fluessig-9",
+    name: "Kokoswasser",
+    weight: "1 l",
+    price: 2.05,
+    image: "img/fluessig-placeholder.png",
+    category: "fluessig-SE"
+  }
+];
+//PRODUCT CARDS GRID PLACES ////////////////////////////////
+const basisGrid = document.getElementById("basis-CEREAL");
+const getrFrueGrid = document.getElementById ("getrocknete-Fr");
+const nueSamGrid = document.getElementById("Nuesse-Samen");
+const superGrid = document.getElementById("super-Foods");
+const extraSuessGrid = document.getElementById("extra-Su");
+const fluessigGrid = document.getElementById("fluessig-SE")
+////////////////////////////////////////////////////////////
+
+//FUNCTION TO CREATE A CARD with innerHTML
+function createProductCard(product) {
+  const card = document.createElement("div");
+  card.classList.add("productCard2");
+
+  // store product info on the card
+  card.dataset.id = product.id;
+//ACTUAL CARD HTML THAT IS TIED TO CSS
+  card.innerHTML = `
     <div class="FlexBadges">
-      <div class="weightBadge2"><p>500g</p></div>
-      <div class="priceBadge2"><p>3,00€</p></div>
+      <div class="weightBadge2"><p>${product.weight}</p></div>
+      <div class="priceBadge2"><p>${product.price.toFixed(2)} €</p></div>
     </div>
 
     <div class="FlexImageTextButton">
       <div class="bowlImage2">
-        <img src="img/haferflockenplaceholder.png" alt="bowlImage2" />
+        <img src="${product.image}" alt="${product.name}" />
       </div>
 
       <div class="headlineCard">
-        <p>Zarte <br />Haferflocken</p>
+        <p>${product.name.replace(" ", "<br />")}</p>
         <button class="btn">Hinzufügen</button>
       </div>
     </div>
-  </div>
-`
+  `;
 
-const basisGrid = document.getElementById('basis-CEREAL')
-
-for (let i = 0; i < 21; i++) {
-  basisGrid.innerHTML += productCardTemplate
+  return card;
 }
-const getrockneteFrGrid = document.getElementById('getrocknete-Fr')
+//PRODUCTCARD INSTALLMENT//////////////////////////////////////
+//Append->Anhängen "Hänge ein untergeordnetes Element an" "forEach theme put the product in this grid"
+basisCerealien.forEach(product => {
+  basisGrid.appendChild(createProductCard(product));
+});
 
-// (sentence 1 ; sentence 2; sentence 3)
-for (let i = 0; i < 13; i++) {
-  getrockneteFrGrid.innerHTML += productCardTemplate
-}
+getrockneteFruechte.forEach(product => {
+  getrFrueGrid.appendChild(createProductCard(product));
+});
 
-const NuesseSamenGrid = document.getElementById('Nuesse-Samen')
+nuesseSamen.forEach(product=> {
+  nueSamGrid.appendChild(createProductCard(product));
+});
 
-for (let i = 0; i < 12; i++) {
-  NuesseSamenGrid.innerHTML += productCardTemplate
-}
+superFoods.forEach(product=> {
+  superGrid.appendChild(createProductCard(product));
+});
 
-const SuperfoodsGrid = document.getElementById('super-Foods')
+extraSuess.forEach(product=> {
+  extraSuessGrid.appendChild(createProductCard(product));
+});
 
-for (let i = 0; i < 11; i++) {
-  SuperfoodsGrid.innerHTML += productCardTemplate
-}
-
-const extraSuessGrid = document.getElementById('extra-Su')
-for (let i = 0; i < 7; i++) {
-  extraSuessGrid.innerHTML += productCardTemplate
-}
-
-const FluessigkeitenGrid = document.getElementById('fluessig-SE')
-
-for (let i = 0; i < 10; i++) {
-  FluessigkeitenGrid.innerHTML += productCardTemplate
-}
-
+fluessigkeiten.forEach(product=> {
+  fluessigGrid.appendChild(createProductCard(product));
+});
+///////////////////////////////////////////////////////////////////
