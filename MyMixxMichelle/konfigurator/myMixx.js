df7c9ae4b491ae7acb923c19b0e35ccc23d5f985
michellevod <michelle.nestler@vodafone.com>
2026-02-03 07:57:58 +0100

    Warenkorb mit ChatGPT angepasst - Preiskalkulation

--- MyMixxMichelle/konfigurator/myMixx.js
+++ MyMixxMichelle/konfigurator/myMixx.js
@@ -22,18 +22,7 @@ setInterval(() => {
   }, 500) // half of CSS transition time
 }, 4000)
 
-// CART ITEM TRASHCAN REMOVAL BUTTON //
-var removeCartItemButton = document.getElementsByClassName('removeButton')
-console.log(removeCartItemButton)
-for (var i = 0; i < removeCartItemButton.length; i++) {
-  var button = removeCartItemButton[i]
-  button.addEventListener('click', function (event) {
-    var buttonClicked = event.target
-    buttonClicked.closest('.cart-item').remove()
-  })
-}
-// HINZUFÜGEN VON EINEM ITEM //
-
+// SHOPPING CART FUNCTIONALITY //
 var addToCartButton = document.getElementsByClassName('addToCart')
 
 for (var i = 0; i < addToCartButton.length; i++) {
@@ -44,28 +33,62 @@ for (var i = 0; i < addToCartButton.length; i++) {
 function addToCartClicked (event) {
   var button = event.target
   var shopItem = button.closest('.productCard2')
+
   var imageSrc = shopItem.getElementsByClassName('productImage')[0].src
   var title = shopItem.getElementsByClassName('cardName')[0].innerText
   var weight = shopItem.getElementsByClassName('weightBadge2')[0].innerText
   var price = shopItem.getElementsByClassName('priceBadge2')[0].innerText
-  console.log(title, imageSrc, weight, price)
+
   addItemToCart(title, imageSrc, weight, price)
 }
 
- function addItemToCart (title, imageSrc, weight, price ) {
- var popOverCartItem = document.createElement ("div")
- popOverCartItem.innerText = title, imageSrc, weight, price
- var cartItems = document.getElementsByClassName("cart-items")[0]
- var CartItemContent = `  <div class="cart-item">
-                <img class="productImage imageFrame"
-                  src="${imageSrc}"
-                />
-                <span class="cart-item-title">${title}</span>
-                <span class="cart-price">${price}</span>
-                 <span class="cart-weight">${weight}</span>
-                  <i class="removeButton fa-solid fa-trash-can"></i>
-                 
-                </div>`
-                popOverCartItem.innerHTML = CartItemContent
-cartItems.append(popOverCartItem)
+function addItemToCart (title, imageSrc, weight, price) {
+  var popOverCartItem = document.createElement("div")
+  var cartItems = document.getElementsByClassName("cart-items")[0]
+
+  var CartItemContent = `  
+    <div class="cart-item">
+      <img class="productImage imageFrame" src="${imageSrc}" />
+      <span class="cart-item-title">${title}</span>
+      <span class="cart-price">${price}</span>
+      <span class="cart-weight">${weight}</span>
+      <i class="removeButton fa-solid fa-trash-can"></i>
+    </div>`
+
+  popOverCartItem.innerHTML = CartItemContent
+  cartItems.append(popOverCartItem)
+
+  // ✅ REMOVE BUTTON FOR THIS ITEM
+  popOverCartItem
+    .getElementsByClassName('removeButton')[0]
+    .addEventListener('click', function (event) {
+      event.target.closest('.cart-item').remove()
+      updateCartTotal()
+    })
+
+  // ✅ UPDATE TOTAL AFTER ADD
+  updateCartTotal()
+}
+
+function updateCartTotal() {
+  var cartItemContainer = document.getElementsByClassName('cart-items')[0]
+  var cartItems = cartItemContainer.getElementsByClassName('cart-item')
+
+  var total = 0
+
+  for (var i = 0; i < cartItems.length; i++) {
+    var cartItem = cartItems[i]
+    var priceElement = cartItem.getElementsByClassName('cart-price')[0]
+    var price = parseFloat(priceElement.innerText.replace('€', '').trim())
+    total += price
+  }
+
+  total = Math.round(total * 100) / 100
+
+  // ✅ UPDATE ALL TOTALS (popover + sticky button)
+  var totalElements = document.getElementsByClassName('cart-total-price')
+
+  for (var i = 0; i < totalElements.length; i++) {
+    totalElements[i].innerText = total.toFixed(2) + '€'
+  }
 }
